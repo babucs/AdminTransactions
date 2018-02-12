92829e96e0df30f034a681eecf3326ead88217cb
babucs <m.babufastmail@gmail.com>
2018-02-12 11:19:57 +0530

    Initial commit

---
+++ src/main/java/fmcg/admin/AdminTransactionsApplication.java
@@ -0,0 +1,12 @@
+package fmcg.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminTransactionsApplication.class, args);
+	}
+}
---
+++ src/test/java/fmcg/admin/AdminTransactionsApplicationTests.java
@@ -0,0 +1,16 @@
+package fmcg.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminTransactionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
